18e55a9faaf8c9de2a266f1164d8bba70055fc6e
Erin <50000677+sinclairems@users.noreply.github.com>
2024-04-01 14:12:31 -0500

    Further file organization, update boilerplate files

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+// from 14.1.22 Student MVC Review in UTA-VIRT-FSF-PT-12-2020-U-LOL
+
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+}
+
+module.exports = sequelize;
